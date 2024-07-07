48631ce6a4bb5899e8be8966393572de8ec01f61
Aaryan Sharda <aaryan.sharda31@gmail.com>
2024-07-07 16:15:45 +0530

    library completed

--- project_library/script.js
+++ project_library/script.js
@@ -1,71 +1,67 @@
-const library = [];
+let myLibrary = [];
 
-function book(title, author, numberOfPages, status) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.status = status;
-};
-
-function addBookToLibrary() {
-
-};
-
-const book2 = new book("No", "Hsna", 3, "Unread")
-
-library.push(book2, book2, book2, book2,book2, book2,book2, book2,book2, book2,book2, book2,)
-console.log(library)
-
-
-
-let container = document.querySelector(".container")
-
-
-library.forEach((item) => {
-    let div = document.createElement("div")
-    container.appendChild(div)
-    div.setAttribute("class", "card")
-    let para = document.createElement("p");
-    para.innerText = `title: ${item.title} \n author: ${item.author} \n Number Of Pages: ${item.numberOfPages} \n Status: ${item.status}`
-    div.appendChild(para);
-})
-
-const dialog = document.querySelector("dialog");
-const titleValue = dialog.querySelector("#title");
-const authorValue = dialog.querySelector("#author");
-const numberOfPagesValue = dialog.querySelector("#number-of-pages");
-const submitButton = dialog.querySelector("#submit");
-const showButton = document.querySelector("dialog + button");
-showButton.addEventListener("click", ()=> {
-    dialog.showModal();
-    
-});
-
-function displayRadioValue() {
-    let ele = document.getElementsByName('status');
-    for (i = 0; i < ele.length; i++) {
-        if (ele[i].checked)
-            return  ele[i].value
-    }
+    this.pages = pages;
+    this.read = read; 
 }
 
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
+}
 
-submitButton.addEventListener("click", (event)=> {
-    event.preventDefault();
-    dialog.close();
-    let book1 = new book(titleValue.value, authorValue.value, numberOfPagesValue.value, displayRadioValue());
-    library.push(book1);
-    let newBook = [library.pop()];
-    newBook.forEach((item) => {
-        let div = document.createElement("div")
-        container.appendChild(div)
-        div.setAttribute("class", "card")
-        let para = document.createElement("p");
-        para.innerText = `title: ${item.title} \n author: ${item.author} \n Number Of Pages: ${item.numberOfPages} \n Status: ${item.status}`
-        div.appendChild(para);
-        })
-    titleValue.value = "";
-    authorValue.value = "";
-    numberOfPagesValue.value = "";
+function toggleRead(index) {
+  myLibrary[index].toggleRead();
+  render()
+}
 
+function render() {
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement("div");
+    bookEl.setAttribute("class", "book-card");
+    bookEl.innerHTML = `
+      <div class="card-header">
+        <h3 class="title">${book.title}</h3>
+        <h5 class="author">by ${book.author}</h5>
+      </div>
+      <div class="card-body">
+        <p>${book.pages} pages</p>
+        <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+        <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+        <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
+      </div>
+    `;
+    libraryEl.appendChild(bookEl);
+  }
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  render()
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  let title = document.querySelector("#title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  render();
+}
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function() {
+   let newBookForm = document.querySelector("#new-book-form");
+   newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function(event){
+  event.preventDefault();
+  addBookToLibrary();
 })
