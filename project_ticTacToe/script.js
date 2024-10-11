741da71ac1b623fd451c4f5569d2a179f8fe434c
Aaryan Sharda <aaryan.sharda31@gmail.com>
2024-10-11 22:11:18 +0530

    Completed TicTacToe ?

--- project_ticTacToe/script.js
+++ project_ticTacToe/script.js
@@ -7,59 +7,93 @@ const box6 = document.querySelector(".box-6");
 const box7 = document.querySelector(".box-7");
 const box8 = document.querySelector(".box-8");
 const box9 = document.querySelector(".box-9");
+const allBoxes = document.querySelectorAll(".grid");
+const winnerDeclaration = document.querySelector(".result");
+const turnText = document.querySelector(".title");
 
+let result = 0;
+turnText.textContent = "Tic - Tac - Toe"
 
 
 function symbol() {
   if(game.getActivePlayer().symbol == "X") {    
     const imgCross = document.createElement("img");
     imgCross.setAttribute("src", "./1553039_blue_letter_x_alphabet_letters_icon.png");
+    imgCross.setAttribute("class", "animate")
     return imgCross;
   } else {
     const imgCircle = document.createElement("img");
     imgCircle.setAttribute("src", "./1553085_blue_letter_o_alphabet_letters_icon.png");
+    imgCircle.setAttribute("class", "animate")
     return imgCircle
   }
 }
 
-
-box1.addEventListener("click", () => {
-  box1.appendChild(symbol());
-  game.playRound(0,0);
-});;
-box2.addEventListener("click", () => {
-  box2.appendChild(symbol());
-  game.playRound(0,1);
-});;
-box3.addEventListener("click", () => {
-  box3.appendChild(symbol());
-  game.playRound(0,2);
-});;
-box4.addEventListener("click", () => {
-  box4.appendChild(symbol());
-  game.playRound(1,0);
-});
-box5.addEventListener("click", () => {
-  box5.appendChild(symbol());
-  game.playRound(1,1);
-});
-box6.addEventListener("click", () => {
-  box6.appendChild(symbol());
-  game.playRound(1,2);
-});;
-box7.addEventListener("click", () => {
-  box7.appendChild(symbol());
-  game.playRound(2,0);
-});;
-box8.addEventListener("click", () => {
-  box8.appendChild(symbol());
-  game.playRound(2,1);
-});;
-box9.addEventListener("click", () => {
-  box9.appendChild(symbol());
-  game.playRound(2,2);
-});;
-
+  box1.addEventListener("click", function placeSymbol(e) {
+    if(result != 1) {
+    box1.appendChild(symbol());
+    if((gameBoard().boardGetter())[0][0] == 0) {
+      e.currentTarget.removeEventListener(e.type, placeSymbol);
+    }
+    game.playRound(0,0);
+    }
+  });
+  box2.addEventListener("click", function placeSymbol(e) {
+    if(result != 1) {
+    box2.appendChild(symbol());
+    game.playRound(0,1);
+    e.currentTarget.removeEventListener(e.type, placeSymbol);
+    }
+  });
+  box3.addEventListener("click", function placeSymbol(e) {
+    if(result != 1) {
+    box3.appendChild(symbol());
+    game.playRound(0,2);
+    e.currentTarget.removeEventListener(e.type, placeSymbol);
+    }
+  });
+  box4.addEventListener("click", function placeSymbol(e) {
+    if(result != 1) {
+    box4.appendChild(symbol());
+    game.playRound(1,0);
+    e.currentTarget.removeEventListener(e.type, placeSymbol);
+    }
+  });
+  box5.addEventListener("click", function placeSymbol(e) {
+    if(result != 1) {
+    box5.appendChild(symbol());
+    game.playRound(1,1);
+    e.currentTarget.removeEventListener(e.type, placeSymbol);
+    }
+  });
+  box6.addEventListener("click", function placeSymbol(e) {
+    if(result != 1) {
+    box6.appendChild(symbol());
+    game.playRound(1,2);
+    e.currentTarget.removeEventListener(e.type, placeSymbol);
+    }
+  });
+  box7.addEventListener("click", function placeSymbol(e) {
+    if(result != 1) {
+    box7.appendChild(symbol());
+    game.playRound(2,0);
+    e.currentTarget.removeEventListener(e.type, placeSymbol);
+    }
+  });
+  box8.addEventListener("click", function placeSymbol(e) {
+    if(result != 1) {
+    box8.appendChild(symbol());
+    game.playRound(2,1);
+    e.currentTarget.removeEventListener(e.type, placeSymbol);
+    }
+  });
+  box9.addEventListener("click", function placeSymbol(e) {
+    if(result != 1) {
+    box9.appendChild(symbol());
+    game.playRound(2,2);
+    e.currentTarget.removeEventListener(e.type, placeSymbol);
+    }
+  });
 
 
 
@@ -93,12 +127,21 @@ function gameBoard() {
       }
     }
   }
-  return {addSymbol, printBoard, boardGetter};
+
+  const eraseBoard = () => {
+    for(i = 0; i <= 2; i++) {
+      for(j = 0; j <= 2; j++) {
+        board[i][j] = 0;
+      }
+    }
+    console.log(board)
+  }
+  return {addSymbol, printBoard, boardGetter, eraseBoard};
 };
 
 function gameController(
   playerOneName = "Player One",
-  playerTwoName = "PlayerTwo"
+  playerTwoName = "Player Two"
 ) {
   const board = gameBoard();
 
@@ -122,6 +165,7 @@ function gameController(
   const printNewRound = () => {
     board.printBoard();
     console.log(`${getActivePlayer().name}'s turn`);
+    turnText.textContent = `${getActivePlayer().name}'s turn`;
   }
 
   const winConditions = () => {
@@ -137,10 +181,10 @@ function gameController(
         }
       }
       if (flagX == 3) {
-        console.log(`Winner is ${getActivePlayer().name}`);
+        winnerDeclaration.textContent = `Winner is ${getActivePlayer().name}`;
         return 1;
       } else if(flagO == 3) {
-        console.log(`Winner is ${getActivePlayer().name}`);
+        winnerDeclaration.textContent = `Winner is ${getActivePlayer().name}`;
         return 1;
       }
       flagX = 0;
@@ -153,27 +197,27 @@ function gameController(
         }
       }
       if (flagX == 3) {
-        console.log(`Winner is ${getActivePlayer().name}`);
+        winnerDeclaration.textContent = `Winner is ${getActivePlayer().name}`;
         return 1;
       } else if(flagO == 3) {
-        console.log(`Winner is ${getActivePlayer().name}`);
+        winnerDeclaration.textContent = `Winner is ${getActivePlayer().name}`;
         return 1;
       }
       flagX = 0;
       flagO = 0
       if((board.boardGetter())[0][0] == "X" && board.boardGetter()[1][1] == "X" && board.boardGetter()[2][2] == "X") {
-        console.log(`Winner is ${getActivePlayer().name}`);
+        winnerDeclaration.textContent = `Winner is ${getActivePlayer().name}`;
         return 1;
       } else if((board.boardGetter())[0][2] == "X" && board.boardGetter()[1][1] == "X" && board.boardGetter()[2][0] == "X") {
-        console.log(`Winner is ${getActivePlayer().name}`);
+        winnerDeclaration.textContent = `Winner is ${getActivePlayer().name}`;
         return 1;
       }
       
       if((board.boardGetter())[0][0] == "O" && board.boardGetter()[1][1] == "O" && board.boardGetter()[2][2] == "O") {
-        console.log(`Winner is ${getActivePlayer().name}`);
+        winnerDeclaration.textContent = `Winner is ${getActivePlayer().name}`;
         return 1;
       } else if((board.boardGetter())[0][2] == "O" && board.boardGetter()[1][1] == "O" && board.boardGetter()[2][0] == "O") {
-        console.log(`Winner is ${getActivePlayer().name}`);
+        winnerDeclaration.textContent = `Winner is ${getActivePlayer().name}`;
         return 1;
       }
       for(j = 0; j < 3; j++) {
@@ -182,7 +226,7 @@ function gameController(
         }
       } 
       if (drawCount == 9) {
-        console.log("Draw!")
+        winnerDeclaration.textContent = `DRAW!`;
         return 1;
       }
     }
@@ -194,24 +238,27 @@ function gameController(
     console.log(`${getActivePlayer().name}'s Turn`)
   }
 
-  startGame();
-
-  const playRound = (row,column) => {
-    console.log(`Putting ${getActivePlayer().name}'s Symbol...`)
-    if(board.addSymbol(row,column, getActivePlayer().symbol) == 1) {
-      console.log("Position already has a symbol in it!")
-    } else if(board.addSymbol(row,column, getActivePlayer().symbol) == 2) {
-      console.log("INVALID POSITION")
-    } else{
-    board.addSymbol(row,column, getActivePlayer().symbol);
-    winConditions();
-    switchActivePlayer();
-    printNewRound();
-    }
-    
+  const restartGame = () => {
+    board.eraseBoard();
+    result = 0;
   }
 
-  return {playRound, getActivePlayer};
+  startGame();
+  const playRound = (row,column) => {
+      if (result != 1) {
+      console.log(`Putting ${getActivePlayer().name}'s Symbol...`)
+      board.addSymbol(row,column, getActivePlayer().symbol);
+      result = winConditions();
+      switchActivePlayer();
+      printNewRound();
+    }
+  }
+  return {playRound, getActivePlayer, restartGame};
 }
 
 const game = gameController()
+
+restart.addEventListener("click" , () => {
+  game.restartGame();
+  
+});  
