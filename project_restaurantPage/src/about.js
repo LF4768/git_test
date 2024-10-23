631a412ff6bed1a5e5c0de25fc8420b3800b3f86
Aaryan Sharda <aaryan.sharda31@gmail.com>
2024-10-24 00:06:10 +0530

    Completed JS for Home

---
+++ project_restaurantPage/src/about.js
@@ -0,0 +1,3 @@
+export function showAbout() {
+    console.log("Hello from About");
+}
--- project_restaurantPage/src/index.js
+++ project_restaurantPage/src/index.js
@@ -1,2 +1,12 @@
 import "./styles.css"
 
+import {showHome} from "./home.js"
+import {showMenu} from "./menu.js"
+import {showAbout} from "./about.js"
+
+const homeButton = document.querySelector(".homeButton");
+const menuButton = document.querySelector(".menuButton");
+const aboutButton = document.querySelector(".aboutButton");
+homeButton.addEventListener("click", () => {showHome()});
+menuButton.addEventListener("click", () => {showMenu()});
+aboutButton.addEventListener("click", () => {showAbout()})
