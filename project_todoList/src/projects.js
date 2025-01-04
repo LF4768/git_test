2d45700a62eb55a8d12d5728765afb58fa2518bf
Aaryan Sharda <aaryan.sharda31@gmail.com>
2025-01-04 23:38:03 +0530

    project deletion syntax

--- project_todoList/src/projects.js
+++ project_todoList/src/projects.js
@@ -1,31 +1,24 @@
-export function projectTemplate() {
+
+
+function projectTemplate() {
     const projects = [];
     const projectArray = () => projects;
     const viewProjects = function() {
         console.log(projects);
     }
-    return {projectArray, viewProjects}
-}
-
-
-function projects() {
-    const project = projectTemplate();
-    const todo = todos();
-    const addProject = function() {
-        project.projectArray().push({
+    const projectAdder = function() {
+        projects.push({
             name: "Project Name",
             todos: [],
         });
     }
-    const viewProject = function() {
-        project.viewProjects();
-    }
-
-    const addTodo = function() {
-        project.projectArray()[0].todos.push(todo.addTodo());
-    }
-    return {addProject, viewProject, addTodo};
+    const projectDeleter = function(index) {
+        projects.splice(index,1)
+    }   
+    return {projectArray, viewProjects, projectAdder, projectDeleter}
 }
+
+
 
 function todos() {
     let todo = {};
@@ -36,13 +29,47 @@ function todos() {
             dueDate: "Due Date",
             priority: "Priority",
             notes: "Notes",
+            status: "Incomplete"
         }
         return todo;
     }
+
     return{addTodo};
 }
 
-const start = projects();
+export function controller() {
+    const project = projectTemplate();
+    const todo = todos();
+
+    const addProject = () => project.projectAdder();
+
+    const viewProject = () => project.viewProjects();
+
+    const addTodo = function() {
+        project.projectArray()[0].todos.push(todo.addTodo());
+    }
+    const statusChanger = function() {
+        project.projectArray()[0].todos[0].status = "Complete"
+
+    }
+
+    const viewTodo = function()  {
+        console.log(project.projectArray()[0].todos[0])
+    }
+
+    const deleteProject = function() {
+        project.projectDeleter(0)
+    }
+
+    return {viewProject, addTodo, statusChanger, addProject, deleteProject, viewTodo};
+}
+
+const start = controller();
+start.addProject();
 start.addProject();
 start.addTodo();
+start.viewProject();
+start.statusChanger();
+start.viewTodo();
+start.deleteProject();
 start.viewProject();
