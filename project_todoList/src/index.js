7b4464cbcbb696bb32774db80bdab29faeb8e9f9
Aaryan Sharda <aaryan.sharda31@gmail.com>
2025-06-15 01:47:15 +0530

    todos rendered

--- project_todoList/src/index.js
+++ project_todoList/src/index.js
@@ -1,74 +1,147 @@
 import {start} from './projects.js';
 
 const projects = document.querySelector(".projects");
+const projectTitle = document.querySelector(".project-title")
+const todos = document.querySelector(".todo-list");
 
 
 let projectButtons;
+let index;
 
 import "./styles.css";
 
 
+
 function render() {
-    for(let i = 0; i < start.viewProject().length; i++) {
-        let div = document.createElement("div");
-        div.innerHTML = start.viewProject()[i].name;
-        div.classList.add("project-button")
-        projects.appendChild(div);
-        div.dataset.id = i;
+    const renderProjects = function() {
+        projects.innerHTML = '';
+        for(let i = 0; i < start.viewProject().length; i++) {
+            let projectHolder = document.createElement("button");
+            projectHolder.textContent += `${start.viewProject()[i].name}`
+            projectHolder.classList.add("project-button")
+            projectHolder.addEventListener("click", () => {
+                projectTitle.textContent = `${start.viewProject()[i].name}`
+                index = i;
+                start.viewTodo(i,0)
+                renderProjects();
+                render().renderTodos();
+            });
+            projects.appendChild(projectHolder)
+        }
+        projectButtons = document.querySelectorAll(".project-button");
     }
-    projectButtons = document.querySelectorAll(".project-button");
+    
+    const renderTodos = function() {
+        todos.innerHTML = '';
+        let todoHolder = document.createElement("div");
+        todoHolder.classList.add("todo");
+
+        if(start.viewTodo(index,0) === undefined) {
+        } else{
+                for(let i = 0; i < start.viewProject()[index].todos.length; i++){
+                let inputCheckbox = document.createElement("input");
+                inputCheckbox.classList.add("todo-checkbox");   
+                inputCheckbox.setAttribute("type","checkbox")
+                todoHolder.appendChild(inputCheckbox);
+
+                let nameInfo = document.createElement("div");
+                nameInfo.classList.add("todo-name-info");
+                nameInfo.textContent = `${start.viewTodo(index,i).name}`;
+                todoHolder.appendChild(nameInfo);
+
+                let dueDateInfo = document.createElement("div");
+                dueDateInfo.classList.add("todo-due-date-info");
+                dueDateInfo.textContent = `${start.viewTodo(index,i).dueDate}`;
+                todoHolder.appendChild(dueDateInfo);
+                todos.appendChild(todoHolder);
+            }
+        }
+        
+    }
+
+
+    return {renderProjects, renderTodos}
 }
 
-render();
-
-projectButtons.forEach((button) => {
-    button.addEventListener("click", (e) => console.log(e.target.dataset.id));
-})
-
-const addProjectButton = document.querySelector(".add-project-button");
-const projectDialogBox = document.querySelector(".add-project-dialog");
-const submitProjectButton = document.querySelector(".submit-project-button");
-const projectName = document.querySelector("#project-name");
-
-
-addProjectButton.addEventListener("click", () => {
-    projectDialogBox.showModal();
-})
-
-projectDialogBox.addEventListener("close", () => {
-    start.addProject(projectName.value)
-})
-
-submitProjectButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    projectDialogBox.close();
-})
-
-const addTaskButton = document.querySelector(".add-task-button");
-const taskDialogBox = document.querySelector(".add-task-dialog");
-const submitTaskButton = document.querySelector(".submit-task-button")
-const taskName = document.querySelector("#task-name");
-const taskDesc = document.querySelector("#task-desc");
-const taskDueDate = document.querySelector("#task-due-date");
-const taskPriority = document.querySelector("#task-priority");
-const taskNotes = document.querySelector("#task-notes");    
-
-addTaskButton.addEventListener("click", () => {
-    taskDialogBox.showModal();
-})
-
-taskDialogBox.addEventListener("close", () => {
-    start.addTodo(taskName.value,taskDesc.value,taskDueDate.value,taskPriority.value,taskNotes.value, "incomplete");
-    console.log(start.viewProject());
-})
-
-submitTaskButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    taskDialogBox.close()
-})
+render().renderProjects();
 
 
 
+
+function dialogBoxes() {
+
+    const newProject = function() {
+        const addProjectButton = document.querySelector(".add-project-button");
+        const projectDialogBox = document.querySelector(".add-project-dialog");
+        const submitProjectButton = document.querySelector(".submit-project-button");
+        const projectName = document.querySelector("#project-name");
+
+
+        addProjectButton.addEventListener("click", () => {
+            projectDialogBox.showModal();
+        })
+
+        projectDialogBox.addEventListener("close", () => {
+            if(projectName.value === "") {
+                window.alert("No Value Given!")
+            } else {
+                start.addProject(projectName.value)
+                render().renderProjects();
+                projectName.value = "";
+            }
+        })
+
+        submitProjectButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            projectDialogBox.close();
+        })
+    }
+    
+    const newTodo = function() {        
+        const addTodoButton = document.querySelector(".add-todo-button");
+        const todoDialogBox = document.querySelector(".add-todo-dialog");
+        const submitTodoButton = document.querySelector(".submit-todo-button")
+        const todoName = document.querySelector("#todo-name");
+        const todoDesc = document.querySelector("#todo-desc");
+        const todoDueDate = document.querySelector("#todo-due-date");
+        const todoPriority = document.querySelector("#todo-priority");
+        const todoNotes = document.querySelector("#todo-notes");    
+
+        addTodoButton.addEventListener("click", () => {
+            todoDialogBox.showModal();
+        })
+
+
+        todoDialogBox.addEventListener("close", () => {
+            let currentTime = new Date();
+            let day = currentTime.getDate();
+            let month = currentTime.getMonth() + 1;
+            let year = currentTime.getFullYear();
+            let date = year + "-" + "0" + month + "-" + day
+            
+            if(todoDueDate.value < date) {
+                window.alert("Due Date cannot be less than current date!")
+            } else if(todoName.value === "" || todoDesc.value === "" || todoPriority.value === "" || todoNotes.value === "" ) {
+                window.alert("No Value Given!")
+            } else {
+                start.addTodo(index, todoName.value,todoDesc.value,todoDueDate.value,todoPriority.value,todoNotes.value, "incomplete");
+                render().renderTodos();
+            }
+            console.log(start.viewProject());
+        })
+
+        submitTodoButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            todoDialogBox.close()
+        })
+    }
+
+    return{newProject, newTodo};
+}
+
+dialogBoxes().newTodo();
+
+    
 
 
 
--- project_todoList/src/projects.js
+++ project_todoList/src/projects.js
@@ -15,10 +15,10 @@ function projectTemplate() {
     const projectDeleter = function(index) {
         projects.splice(index,1)
     }   
-    function indexFinder() {
+    function indexFinder(name) {
         let index = 0;
         for(let i = 0; i < projectArray().length; i++) {
-            if(projectArray()[i].name == "asdf") {
+            if(projectArray()[i].name == name) {
                 index = i;
                 break;
             } 
@@ -40,6 +40,13 @@ function todos() {
         this.notes = notes;
         this.status = status;
     }
+    todoDetails.prototype.toggleStatus = function() {
+        if(this.status == "incomplete") {
+            this.status = "complete";
+        } else {
+            this.status = "incomplete";
+        }
+    }
 
     const addTodo = function(name, desc, dueDate, priority, notes, status) {
         return new todoDetails(name, desc, dueDate, priority, notes, status)
@@ -56,22 +63,22 @@ function controller() {
 
     const viewProject = () => project.viewProjects();
 
-    const addTodo = function(name, desc, dueDate, priority, notes, status) {
-        let index = project.indexFinder();
-        project.projectArray()[index].todos.push(todo.addTodo(name, desc, dueDate, priority, notes, status));
+    const addTodo = function(index, name, desc, dueDate, priority, notes, status) {
+        project.projectArray()[index].todos.push(todo.addTodo(name, desc, dueDate, priority, notes, status)); 
     }
     const statusChanger = function(todoIndex) {
         let projectIndex = project.indexFinder();
-        project.projectArray()[projectIndex].todos[todoIndex].status = "Complete"
+        project.projectArray()[projectIndex].todos[todoIndex].toggleStatus();
 
     }
 
     const viewTodo = function(index1, index2)  {
-        console.log(project.projectArray()[index1].todos[index2])
+        return project.projectArray()[index1].todos[index2]
     }
 
     const deleteProject = function(index) {
         project.projectDeleter(index)
+        console.log(start.viewProject())
     }
 
     return {viewProject, addTodo, statusChanger, addProject, deleteProject, viewTodo};
@@ -81,8 +88,9 @@ export const start = controller();
 start.addProject("hello");
 start.addProject("asdf");
 start.addProject("gfddfgh");
-start.addTodo('x', "1", "3", "1", "hello", "incomplete");
-start.addTodo('y', "5", "26", "000", "bye", "complete");
+start.addTodo(0, 'x', "1", "3", "1", "hello", "incomplete");
+start.addTodo(1, 'y', "5", "26", "000", "bye", "incomplete");
+start.addTodo(1, 'y', "5", "26", "000", "bye", "incomplete");
 console.log(start.viewProject())
-start.statusChanger(1);
+start.statusChanger(0);
 start.viewTodo(1,0);
